0122dd267b5346493e6afc229fd0e0061573900c
rlflorencio <rafaellflorencio@outlook.com>
2022-11-18 13:41:48 -0300

    Cria metodo de busca por Id

--- src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
@@ -13,8 +13,17 @@ public class ParkingService {
 
     static {
         var id = getUUID();
+        var id1 = getUUID();
+        var id2 = getUUID();
+        var id3 = getUUID();
         Parking parking = new Parking(id, "PGK-8876", "PE", "FIAT PALIO", "PRATA");
+        Parking parking1 = new Parking(id1, "PCF-2955", "PE", "HYUNDAI HB20", "BRANCO");
+        Parking parking2 = new Parking(id2, "PGK-3447", "PE", "HONDA CIVIC", "CINZA");
+        Parking parking3 = new Parking(id3, "PGK-8776", "PE", "PEUGEOT 207 SEDAN", "VERDE");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
+        parkingMap.put(id2, parking2);
+        parkingMap.put(id3, parking3);
     }
 
     public List<Parking> findAll(){
@@ -23,5 +32,9 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
     }
 }
