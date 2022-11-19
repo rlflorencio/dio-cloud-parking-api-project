a658b92db1c6825f6b66606c1463dcb1a3b9f90b
rlflorencio <rafaellflorencio@outlook.com>
2022-11-19 10:43:42 -0300

    Enabled TestContainers

--- src/test/java/one/digitalinnovation/cloudparking/service/ParkingCheckOutTest.java
+++ src/test/java/one/digitalinnovation/cloudparking/service/ParkingCheckOutTest.java
@@ -1,12 +1,31 @@
 package one.digitalinnovation.cloudparking.service;
 
+import io.restassured.RestAssured;
+import one.digitalinnovation.cloudparking.controller.AbstractContainerBase;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ParkingCheckOutTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingCheckOutTest extends AbstractContainerBase {
 
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest(){
+        System.out.println(randomPort);
+        RestAssured.port = randomPort;
+    }
     @Test
     void getBill() {
+        RestAssured.given()
+                .auth()
+                .basic("user", "Teste@123")
+                .when()
+                .post("/{id}")
     }
 }
